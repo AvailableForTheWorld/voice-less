a5b4f1b50d09cb8c1e64f2df773ebc58858371ae
AvailableForTheWorld <49779788+AvailableForTheWorld@users.noreply.github.com>
2022-09-02 11:20:16 +0800

    Revert "部署vercel修改"

--- vite.config.ts
+++ vite.config.ts
@@ -38,9 +38,15 @@ export default defineConfig(({ mode }) => ({
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
   build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["cjs", "es"],
+      fileName: "index",
+    },
     sourcemap: mode === "production",
     outDir: "dist",
     rollupOptions: {
+      external: ["@netless/fastboard", "@netless/window-manager", "white-web-sdk"],
       output: {
         manualChunks: undefined,
         inlineDynamicImports: true,
